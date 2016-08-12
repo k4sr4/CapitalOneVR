acb8a9289a0af020d44234b98ecf6e56a58ed439
singh14sandeep <singh14sandeep@gmail.com>
2016-08-12 13:49:55 -0500

    Test semi colon

--- Assets/Scripts/CapOneFloatingObjInit.cs
+++ Assets/Scripts/CapOneFloatingObjInit.cs
@@ -5,7 +5,7 @@ public class CapOneFloatingObjInit : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		this.transform.localScale = new Vector3 (2, 5, 5);
+		this.transform.localScale = new Vector3 (2, 5, 5);;
 	}
 	
 	// Update is called once per frame
